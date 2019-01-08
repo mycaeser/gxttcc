2576cdcbbe8a01b4e745bd08548679b8213ea718
DESKTOP-UJ40NKO\Caeseruu <uxcaeser@mail.com>
2019-01-08 19:13:28 +0800

    底部名称显示问题已解决

--- src/main/webapp/resources/js/cps/cjoinus.js
+++ src/main/webapp/resources/js/cps/cjoinus.js
@@ -13,26 +13,26 @@ $(function(){
 		});//
 		$('#hr-part').html('<a href="/gxttcc/addarticlemodel/addteam?a=1" >新增地址</a><br /><br />'+outHtml1+outHtml2);
 	});
-	outHtml1='<tr><th>网站名称</th><th>备案号</th><th>邮箱</th><th>编辑</th></tr><tr>';
-	outHtml2='';
+	var outHtml3='<tr><th>网站名称</th><th>备案号</th><th>邮箱</th><th>编辑</th></tr><tr>';
+	var outHtml4='';
 	
 	$.getJSON(getAddressUrl+1,function(data){
 		var newsObj=data.add;
 		
-		outHtml2='<tr><td>'+newsObj.addName+'</td><td>'+newsObj.addContent+'</td><td>'+newsObj.addEmail+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj.aac101+'" >编辑</a></td></tr>';
+		outHtml4='<tr><td>'+newsObj.addName+'</td><td>'+newsObj.addContent+'</td><td>'+newsObj.addEmail+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj.aac101+'" >编辑</a></td></tr>';
 		
-		$('#bot-index').html(outHtml1+outHtml2);
+		$('#bot-index').html(outHtml3+outHtml4);
 	});
-	outHtml1='<tr><th>公司名</th><th>地址</th><th>联系人</th><th>电话</th><th>编辑</th></tr><tr>';
-	outHtml2='';
+	var outHtml5='<tr><th>公司名</th><th>地址</th><th>联系人</th><th>电话</th><th>编辑</th></tr><tr>';
+	var outHtml6='';
 	$.getJSON(getAddressUrl+2,function(data){
 		var newsObj1=data.add;
-		outHtml2='<tr><td>'+newsObj1.addName+'</td><td>'+newsObj1.addContent+'</td><td>'+newsObj1.addPhoneOwner+'</td>><td>'+newsObj1.addPhoneNumber+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj1.aac101+'" >编辑</a></td></tr>';
-		$('#bot-index').html(outHtml1+outHtml2);
+		outHtml6='<tr><td>'+newsObj1.addName+'</td><td>'+newsObj1.addContent+'</td><td>'+newsObj1.addPhoneOwner+'</td>><td>'+newsObj1.addPhoneNumber+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj1.aac101+'" >编辑</a></td></tr>';
+		//$('#bot-part').html(outHtml1+outHtml2);
 		$.getJSON(getAddressUrl+3,function(data){
 			var newsObj2=data.add;
-			outHtml2+='<tr><td>'+newsObj2.addName+'</td><td>'+newsObj2.addContent+'</td><td>'+newsObj2.addPhoneOwner+'</td>><td>'+newsObj2.addPhoneNumber+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj2.aac101+'" >编辑</a></td></tr>';
-			$('#bot-index').html(outHtml1+outHtml2);
+			outHtml6+='<tr><td>'+newsObj2.addName+'</td><td>'+newsObj2.addContent+'</td><td>'+newsObj2.addPhoneOwner+'</td>><td>'+newsObj2.addPhoneNumber+'</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/gxttcc/cps/brief?b=1&c='+newsObj2.aac101+'" >编辑</a></td></tr>';
+			$('#bot-part').html(outHtml5+outHtml6);
 		});
 	});
 })
